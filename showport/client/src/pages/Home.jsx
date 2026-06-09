69feb63732ff35931f521b304442ea9f87b2a23b
the developer <eloidev10@gmail.com>
2026-06-09 08:09:36 -0700

    Create Home.jsx

---
+++ showport/client/src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Home(){
+
+return(
+
+<div>
+
+<section className="hero">
+
+<h1>
+Showcase Your Teaching Journey
+</h1>
+
+<p>
+Create professional teaching portfolios
+and connect with educators worldwide.
+</p>
+
+<button>
+Create Portfolio
+</button>
+
+</section>
+
+</div>
+
+);
+
+}
